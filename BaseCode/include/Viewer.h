a15e7f5f7b00434fc358297181829868b977cf1b
larc <luccianorc@gmail.com>
2016-10-26 09:38:06 -0200

    fix compile basecode and hot2

--- BaseCode/include/Viewer.h
+++ BaseCode/include/Viewer.h
@@ -10,7 +10,13 @@
 #ifndef DDG_VIEWER_H
 #define DDG_VIEWER_H
 
+#ifdef __APPLE__
 #include <GLUT/glut.h>
+#else
+#include <GLES3/gl3.h>
+#include <GL/glut.h>
+#include <GL/freeglut.h>
+#endif
 #include "Mesh.h"
 #include "Camera.h"
 #include "Shader.h"
